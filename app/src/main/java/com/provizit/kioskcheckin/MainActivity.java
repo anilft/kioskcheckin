ece62e1cb6b1dde7c22004c57e37fe71e2de780b
D Murthy <dmurthy@provizit.com>
2022-12-19 10:42:52 +0530

    project change

--- app/src/main/java/com/provizit/kioskcheckin/MainActivity.java
+++ app/src/main/java/com/provizit/kioskcheckin/MainActivity.java
@@ -1,8 +1,7 @@
 package com.provizit.kioskcheckin;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,13 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(MainActivity.this,Loginactivity.class);
-                MainActivity.this.startActivity(intent);
-                MainActivity.this.finish();
-            }
-        },5000);
     }
 }
